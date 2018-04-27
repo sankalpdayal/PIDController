b230fc69ec996ee01a97a9bad5e9ce0176b4767c
Sankalp Dayal <sadayal@invensense.com>
2018-04-27 00:29:24 -0700

    Adding parameter reading function

--- src/Twiddle.cpp
+++ src/Twiddle.cpp
@@ -12,6 +12,8 @@ Twiddle::~Twiddle() {}
 
 void Twiddle::Init(){
 	std::cout << "Twiddle init"<<std::endl;
+	if (this->ReadParameters())
+		return;
 	iter = 0;
 	for (int i=0; i<3 ;i++){
 		p[i] = 0.0;
@@ -145,40 +147,28 @@ bool Twiddle::ReadParameters(){
 	bool filefound = false;
     if(fin.is_open()) {
 		filefound = true;
-        fin.seekg(-1,ios_base::end);                // go to one spot before the EOF
-
-        bool keepLooping = true;
-        while(keepLooping) {
-            char ch;
-            fin.get(ch);                            // Get current byte's data
-
-            if((int)fin.tellg() <= 1) {             // If the data was at or before the 0th byte
-                fin.seekg(0);                       // The first line is the last line
-                keepLooping = false;                // So stop there
-            }
-            else if(ch == '\n') {                   // If the data was a newline
-                keepLooping = false;                // Stop at the current position.
-            }
-            else {                                  // If the data was neither a newline nor at the 0 byte
-                fin.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
-            }
-        }
-
-        std::string s;            
-        getline(fin,s);                      // Read the current line
+        std::string s, lastline;
+		while (std::getline(fin, s))
+		{
+			if (s.length() > 5)
+			{
+				lastline = s;
+			}
+			// process pair (a,b)
+		}
 
         fin.close();
-		
 		size_t pos = 0;
 		std::string token;
 		double temp[8];
 		int ind = 0;
 		std::string delimiter = ",";
-		while ((pos = s.find(delimiter)) != std::string::npos) {
-			token = s.substr(0, pos);
+		lastline += ",";
+		while ((pos = lastline.find(delimiter)) != std::string::npos) {
+			token = lastline.substr(0, pos);
 			temp[ind] = atof(token.c_str());
 			ind++;
-			s.erase(0, pos + delimiter.length());
+			lastline.erase(0, pos + delimiter.length());
 		}
 		p[0] = temp[0];
 		p[1] = temp[1];
